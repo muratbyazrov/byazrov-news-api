6a3294c6c4807d1922bf80b6065fbb2f9bfdc90e
Murat Byazrov <murat.byazrov@yandex.ru>
2020-07-01 11:20:39 +0300

    Пользователи могут удалять только свои карточки

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -3,6 +3,7 @@ const Article = require('../models/article');
 
 // Подключим ошибку (без деструктуризации)
 const NotFounError = require('../errors/not-found-err');
+const Forbidden = require('../errors/forbidden');
 
 // обработчик, отдающий все карточки
 module.exports.getArticles = (req, res, next) => {
@@ -13,6 +14,7 @@ module.exports.getArticles = (req, res, next) => {
     .catch(next);
 };
 
+// Обработчик удаления создания статей
 module.exports.createArticle = (req, res, next) => {
   const {
     keyword, title, text, date, source, link, image,
@@ -24,10 +26,20 @@ module.exports.createArticle = (req, res, next) => {
     .catch(next);
 };
 
+// Обработчик удаления карточек
 module.exports.deleteArticle = async (req, res, next) => {
   await Article.findById(req.params.articleId)
+    .select('+owner')
     .orFail(new NotFounError('Такой статьи нет'))
-    .then((article) => article.remove()
-      .then(res.send({ data: article })))
+    .then((article) => {
+      // JSON в строку - берем поле owner и сравниваем с id пользователя
+      if (JSON.stringify(article.owner) !== JSON.stringify(req.user._id)) {
+        console.log(article, req.user._id);
+        // генерируется ошибка и переходим в след. обработчик ошибки
+        throw new Forbidden('Нельзя удалить чужую статью');
+      }
+      return article.remove()
+        .then(res.send({ data: article }));
+    })
     .catch(next);
 };
