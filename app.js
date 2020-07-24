cbd67f2b04a233cfa64309ad10444b44e4c404c7
Murat Byazrov <murat.byazrov@yandex.ru>
2020-07-24 20:09:29 +0300

    Исправил блокировку CORS при поытке отправить запрос от авторизованного пользователя

--- app.js
+++ app.js
@@ -45,11 +45,11 @@ app.use(limiter);
 // Собрали приходящие пакеты в json
 app.use(bodyParser.json());
 
-// разрешаем запросы с фронтенда
+// разрешаем запросы с фронтенда/ oPTIONS - не мог взять куки - CORS жаловался
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE, OPTIONS');
 
   next();
 });
