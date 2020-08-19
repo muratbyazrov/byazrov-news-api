815bf192dffeeb9008ea3fa9aec215398533df51
Murat Byazrov <murat.byazrov@yandex.ru>
2020-08-19 10:05:53 +0300

    Разрешил запросы с фронтенда на сервере попытка 17

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ app.use(cors(corsOptions)); */
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
 
