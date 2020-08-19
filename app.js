0775fa0251069f6d81648b878eb06d86763b69e2
Murat Byazrov <murat.byazrov@yandex.ru>
2020-08-19 09:57:58 +0300

    Разрешил запросы с фронтенда на сервере попытка 15

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', '*');
   }
 
   next();
